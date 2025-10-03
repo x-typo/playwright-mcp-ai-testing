fd32da26aa6bfbaec76dccfc46af95e4873119ce
x-typo <87083014+x-typo@users.noreply.github.com>
2025-10-03 14:37:09 -0600

    Add locators and interaction methods for account details and change password tabs in Profile Settings Page

--- tests/playwright/pages/profile-settings.page.ts
+++ tests/playwright/pages/profile-settings.page.ts
@@ -10,10 +10,28 @@ export class ProfileSettingsPage extends BasePage {
   get pageHeading(): Locator {
     return this.heading("Profile settings");
   }
+  get accountDetailsTab(): Locator {
+    return this.testIdSelector("account-details");
+  }
+  get changePasswordTab(): Locator {
+    return this.testIdSelector("change-password");
+  }
+  get userIdInput(): Locator {
+    return this.testIdSelector("user-id");
+  }
+  get currentPasswordInput(): Locator {
+    return this.testIdSelector("current-password");
+  }
   // ===== LOCATOR METHODS =====
   // ===== NAVIGATIONS =====
   async navigateProfileSettingsPage(): Promise<void> {
     await this.navigatePage("/notes/app/profile");
   }
   // ===== INTERACTIONS =====
+  async openAccountDetailsTab(): Promise<void> {
+    await this.selectTestIdSelector("account-details");
+  }
+  async openChangePasswordTab(): Promise<void> {
+    await this.selectTestIdSelector("change-password");
+  }
 }
