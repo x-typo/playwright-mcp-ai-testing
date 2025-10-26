f56ef08c1f1d47cedb5aff5e1bd96a11707836a3
x-typo <87083014+x-typo@users.noreply.github.com>
2025-10-25 21:52:01 -0600

    update 1

--- tests/playwright/features/practices.test.ts
+++ tests/playwright/features/practices.test.ts
@@ -63,7 +63,7 @@ test.describe("Practices page", () => {
     });
   });
 
-  test.fixme(
+  test.skip(
     "Visual Test",
     { tag: ["@visual", "@smoke", "@regression"] },
     async ({ practicesPage }) => {
